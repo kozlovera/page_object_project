19ba55bc3f0d76090f67905a480d0d8671f03347
Vera Kozlova <kozlovera@tt.tt>
2022-04-30 20:58:19 -0700

    final commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,16 +13,13 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         cur_url = self.browser.current_url
-        # реализуйте проверку на корректный url адрес
         assert 'login/' in cur_url, "URL does not contain login-substring"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
     def register_new_user(self, email, password):
--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,7 @@ from .pages.basket_page import BasketPage
 import time
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
- #                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+  #                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
   #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
   #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
   #                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
@@ -15,6 +15,8 @@ import time
   #                                pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
   #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
   #                                "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -49,12 +51,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -80,10 +84,11 @@ class TestUserAddToBasketFromProductPage:
 
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-        product_page = ProductPage(browser, link)
-        product_page.open()
-        product_page.should_not_be_success_message()
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         product_page = ProductPage(browser, link)
